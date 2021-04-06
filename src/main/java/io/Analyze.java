3b6cf4744822b6cd9c6b3a883a4c9191aae1b977
Айдер <emage.haf@gmail.com>
2021-04-06 17:39:01 +0300

    2. Анализ доступности сервера. [#148113]

--- src/main/java/io/Analyze.java
+++ src/main/java/io/Analyze.java
@@ -1,31 +1,42 @@
 package io;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+//нужно собрать все то что напарсили в список и передать его в метод для записи
 
 public class Analyze {
+
+    private void writingToFile(List<String> list, String target) {
+        try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+            for (int i = 0; i <= list.size(); i += 2) {
+                out.println("beginning at " + list.get(i) + " end at " + list.get(i + 1));
+            }
+        } catch (Exception e) {
+            System.out.println("Ошибка записи в файл");
+        }
+    }
+
     public void unavailable(String source, String target) {
-        StringBuilder stringBuilder = new StringBuilder();
+        List<String> list = new ArrayList<>();
         boolean readyToLineSeparator = true;
         try (BufferedReader reader = new BufferedReader(new FileReader(source))) {
             String s;
             while ((s = reader.readLine()) != null) {
-                String[] strings = s.split(" ");
-                int type = Integer.parseInt(strings[0]);
+                String[] split = s.split(" ");
+                int type = Integer.parseInt(split[0]);
                 if ((type == 400 || type == 500) && readyToLineSeparator) {
-                    stringBuilder.append("beginning ").append(strings[1]).append("; ");
+                    list.add(split[1]);
                     readyToLineSeparator = false;
                 } else if ((type == 300 || type == 200) && !readyToLineSeparator) {
-                    stringBuilder.append("end ").append(strings[1]).append(";").append(System.lineSeparator());
+                    list.add(split[1]);
                     readyToLineSeparator = true;
-                    try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
-                        out.println(stringBuilder);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
                 }
             }
         } catch (IOException ioException) {
             System.out.println("Ошибка ввода-вывода");
         }
+        writingToFile(list, target);
     }
 }
--- src/test/java/io/AnalyzeTest.java
+++ src/test/java/io/AnalyzeTest.java
@@ -2,9 +2,6 @@ package io;
 
 import org.junit.Test;
 
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.*;
-
 public class AnalyzeTest {
 
     @Test
