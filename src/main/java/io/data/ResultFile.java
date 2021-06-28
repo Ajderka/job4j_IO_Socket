d576c8f0a9a98487f7c3e31f449c21b48ce34b61
Айдер <emage.haf@gmail.com>
2021-06-28 11:09:03 +0300

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/io/data/ResultFile.java
@@ -0,0 +1,21 @@
+package io.data;
+
+import java.io.FileOutputStream;
+import java.nio.charset.StandardCharsets;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            String z;
+            for (int i = 1; i <= 10; i++) {
+                for (int j = 1; j <= 10; j++) {
+                    z = Integer.toString(i * j );
+                    out.write((z + " ").getBytes(StandardCharsets.UTF_8));
+                }
+                out.write(System.lineSeparator().getBytes(StandardCharsets.UTF_8));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
