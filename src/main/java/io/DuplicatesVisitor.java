64ea08a856930d556e6b20c65cb2f5c4f515d060
Айдер <emage.haf@gmail.com>
2021-08-08 20:01:20 +0300

    4.2. Поиск дубликатов [#315066 #187128] fixed

--- src/main/java/io/DuplicatesVisitor.java
+++ src/main/java/io/DuplicatesVisitor.java
@@ -5,11 +5,30 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
+    private FileProperty fileProperty;
+    private List<FileProperty> propertyList;
+    private List<FileProperty> duplicate;
+
+    public DuplicatesVisitor() {
+        propertyList = new ArrayList<>();
+        duplicate = new ArrayList<>();
+    }
+
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        System.out.println(file.toAbsolutePath());
+        if (!attrs.isDirectory()) {
+            fileProperty = new FileProperty(attrs.size(), file.getFileName().toString());
+            if (propertyList.contains(fileProperty)) {
+                duplicate.add(fileProperty);
+                System.out.println(fileProperty.getName());
+            } else {
+                propertyList.add(fileProperty);
+            }
+        }
         return super.visitFile(file, attrs);
     }
 }
