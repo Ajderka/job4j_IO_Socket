0ae17b9c2a910354dcbc79719ccbe26ef13e4e2e
Айдер <emage.haf@gmail.com>
2021-08-10 21:32:39 +0300

    4.2. Поиск дубликатов [#315066 #187128] fixed 2

--- src/main/java/io/DuplicatesVisitor.java
+++ src/main/java/io/DuplicatesVisitor.java
@@ -6,28 +6,26 @@ import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-    private FileProperty fileProperty;
-    private List<FileProperty> propertyList;
+    private HashSet<FileProperty> propertySet;
     private List<FileProperty> duplicate;
 
     public DuplicatesVisitor() {
-        propertyList = new ArrayList<>();
+        propertySet = new HashSet<>();
         duplicate = new ArrayList<>();
     }
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        if (!attrs.isDirectory()) {
-            fileProperty = new FileProperty(attrs.size(), file.getFileName().toString());
-            if (propertyList.contains(fileProperty)) {
-                duplicate.add(fileProperty);
-                System.out.println(fileProperty.getName());
-            } else {
-                propertyList.add(fileProperty);
-            }
+        FileProperty fileProperty = new FileProperty(attrs.size(), file.getFileName().toString());
+        if (propertySet.contains(fileProperty) && !duplicate.contains(fileProperty)) {
+            duplicate.add(fileProperty);
+            System.out.println(fileProperty.getName());
+        } else {
+            propertySet.add(fileProperty);
         }
         return super.visitFile(file, attrs);
     }
