01aaa14212f9657239382581574b1100212d22ac
Айдер <emage.haf@gmail.com>
2021-09-28 13:42:16 +0300

    1. Бот [#7921]

--- src/main/java/io/socket/EchoClient.java
+++ src/main/java/io/socket/EchoClient.java
@@ -1,6 +1,5 @@
 package io.socket;
 
-
 import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
@@ -9,7 +8,7 @@ public class EchoClient {
     public static void main(String[] args) throws IOException {
         Socket socket = new Socket("127.0.0.1", 9000);
         OutputStream outputStream = socket.getOutputStream();
-        outputStream.write("Hello".getBytes(StandardCharsets.UTF_8));
+        outputStream.write("Bye".getBytes(StandardCharsets.UTF_8));
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         for (String str = bufferedReader.readLine(); str != null && !str.isEmpty(); str = bufferedReader.readLine()) {
             System.out.println(str);
--- src/main/java/io/socket/EchoServer.java
+++ src/main/java/io/socket/EchoServer.java
@@ -3,30 +3,29 @@ package io.socket;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
+                System.out.println("Client is here");
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.write("Hello, dear friend.".getBytes());
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
                         if (str.contains("Hello\r\n")) {
-                            out.write("Hello".getBytes(StandardCharsets.UTF_8));
-                            out.flush();
+                            out.write("Hello".getBytes());
                         } else if (str.contains("Exit\r\n")) {
-                            out.write("Bye - bye".getBytes(StandardCharsets.UTF_8));
-                            out.flush();
+                            out.write("Bye - bye".getBytes());
                             socket.close();
                             server.close();
                         } else {
-                            out.write("What\r\n".getBytes(StandardCharsets.UTF_8));
-                            out.flush();
+                            out.write("What\r\n".getBytes());
                         }
+                        out.flush();
                     }
                 }
             }
