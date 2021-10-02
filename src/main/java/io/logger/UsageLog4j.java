89274078e934b09978b50e0c6f8af190ed394767
Айдер <emage.haf@gmail.com>
2021-10-02 18:36:18 +0300

    2. Simple Loggin Facade 4 Java. [#268849]

---
+++ src/main/java/io/logger/UsageLog4j.java
@@ -0,0 +1,18 @@
+package io.logger;
+
+import org.apache.log4j.BasicConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UsageLog4j {
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+
+    public static void main(String[] args) {
+        BasicConfigurator.configure();
+        LOG.trace("trace message");
+        LOG.debug("debug message");
+        LOG.info("info message");
+        LOG.warn("warn message");
+        LOG.error("error message");
+    }
+}
