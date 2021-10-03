b8e5e92d78f0db6343f3358f390a837c774cde52
Айдер <emage.haf@gmail.com>
2021-10-03 16:48:04 +0300

    Bot 03.10.2021

--- src/main/java/io/socket/EchoServer.java
+++ src/main/java/io/socket/EchoServer.java
@@ -3,6 +3,7 @@ package io.socket;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
@@ -15,14 +16,23 @@ public class EchoServer {
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        if (str.contains("Hello\r\n")) {
+                        if (str.contains("Hello")) {
                             out.write("Hello, dear friend.".getBytes());
-                        } else if (str.contains("Exit\r\n")) {
+                        } else if (str.contains("Exit")) {
                             out.write("Bye - bye".getBytes());
-                            socket.close();
                             server.close();
                         } else {
-                            out.write("What\r\n".getBytes());
+                            if (str.contains("=")) {
+                                String[] strings = str.split("=");
+                                StringBuilder stringBuilder = new StringBuilder();
+                                for (char c : strings[1].toCharArray()) {
+                                    if (c == ' ') {
+                                        break;
+                                    }
+                                    stringBuilder.append(c);
+                                }
+                                out.write(stringBuilder.toString().getBytes(StandardCharsets.UTF_8));
+                            }
                         }
                         out.flush();
                     }
