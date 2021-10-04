d6326702d5ce01afef7c53059810a66987c33ada
Айдер <emage.haf@gmail.com>
2021-10-04 09:43:28 +0300

    Bot 04.10.2021

--- src/main/java/io/logger/UsageLog4j.java
+++ src/main/java/io/logger/UsageLog4j.java
@@ -9,15 +9,10 @@ public class UsageLog4j {
 
     public static void main(String[] args) {
         BasicConfigurator.configure();
-        short nameLength = 5;
-        int age = 34;
-        byte bloodType = 1;
-        long salary = 23888499L;
-        char shift = 'A';
-        float weight = 84.3F;
-        double growth = 180.5;
-        boolean man = true;
-
-        LOG.debug("User info name length : {}, age : {}, bloodType : {}, salary : {}, shift : {}, weight : {}, growth = {}, man = {}", nameLength, age, bloodType, salary, shift, weight, growth, man);
+        try {
+            throw new Exception("Not supported code");
+        } catch (Exception e) {
+            LOG.error("Exception in log example", e);
+        }
     }
 }
--- src/main/java/io/socket/EchoServer.java
+++ src/main/java/io/socket/EchoServer.java
@@ -3,7 +3,6 @@ package io.socket;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
@@ -16,26 +15,19 @@ public class EchoServer {
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        if (str.contains("Hello")) {
-                            out.write("Hello, dear friend.".getBytes());
-                        } else if (str.contains("Exit")) {
-                            out.write("Bye - bye".getBytes());
-                            server.close();
-                        } else {
-                            if (str.contains("=")) {
-                                String[] strings = str.split("=");
-                                StringBuilder stringBuilder = new StringBuilder();
-                                for (char c : strings[1].toCharArray()) {
-                                    if (c == ' ') {
-                                        break;
-                                    }
-                                    stringBuilder.append(c);
-                                }
-                                out.write(stringBuilder.toString().getBytes(StandardCharsets.UTF_8));
+                        if (str.contains("?msg=")) {
+                            if (str.contains("Hello")) {
+                                out.write("Hello, dear friend.".getBytes());
+                            } else if (str.contains("Exit")) {
+                                out.write("Bye - bye".getBytes());
+                                server.close();
+                            } else {
+                                out.write("What".getBytes());
                             }
                         }
-                        out.flush();
                     }
+                    out.flush();
+                    server.close();
                 }
             }
         }
