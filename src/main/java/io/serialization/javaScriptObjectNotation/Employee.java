498353c44298b4a938d746fbb0a910e984e546e8
Айдер <emage.haf@gmail.com>
2021-10-07 19:47:11 +0300

    2. Формат JSON [#313164]

---
+++ src/main/java/io/serialization/javaScriptObjectNotation/Employee.java
@@ -0,0 +1,77 @@
+package io.serialization.javaScriptObjectNotation;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.Arrays;
+
+public class Employee {
+    private String name;
+    private Boolean married;
+    private int salary;
+    private String[] skills;
+    private Department department;
+
+    public Employee(String name, Boolean married, int salary, String[] skills, Department department) {
+        this.name = name;
+        this.married = married;
+        this.salary = salary;
+        this.skills = skills;
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{"
+                + "name='" + name + '\''
+                + ", married=" + married
+                + ", salary=" + salary
+                + ", skills=" + Arrays.toString(skills)
+                + ", department=" + department
+                + '}';
+    }
+
+    public static void main(String[] args) {
+        Department department = new Department("IT", 56);
+        String[] skills = new String[]{"Communication", "Teamwork", "Problem-solving", "Stress management"};
+        Employee employee = new Employee("Ivan", true, 3600, skills, department);
+        System.out.println(employee);
+        final Gson gson = new GsonBuilder().create();
+        System.out.println(gson.toJson(employee));
+
+        final String employ =
+                "{"
+                        + "\"name\":\"Ivan\","
+                        + "\"married\":true,"
+                        + "\"salary\":3600,"
+                        + "\"skills\":"
+                        + "[\"Communication\",\"Teamwork\",\"Problem-solving\",\"Stress management\"],"
+                        + "\"department\":"
+                        + "{"
+                        + "\"title\":\"IT\","
+                        + "\"numberOfEmployees\":56"
+                        + "},"
+                        + "}";
+
+        final Employee employee1 = gson.fromJson(employ, Employee.class);
+        System.out.println(employee1);
+    }
+}
+
+class Department {
+    private String title;
+    private int numberOfEmployees;
+
+    public Department(String title, int numberOfEmployees) {
+        this.title = title;
+        this.numberOfEmployees = numberOfEmployees;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{"
+                + "title='" + title + '\''
+                + ", numberOfEmployees=" + numberOfEmployees
+                + '}';
+    }
+}
