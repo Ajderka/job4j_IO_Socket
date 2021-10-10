1004bbeb6512831b77613a1a1b585ae1e147b91c
Айдер <emage.haf@gmail.com>
2021-10-10 10:23:19 +0300

    4. JAXB. Преобразование XML в POJO. [#315063]

--- src/main/java/io/serialization/json/Person.java
+++ src/main/java/io/serialization/json/Person.java
@@ -1,12 +1,35 @@
 package io.serialization.json;
 
+import io.logger.UsageLog4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.annotation.*;
+import java.io.StringWriter;
 import java.util.Arrays;
 
+@XmlRootElement(name = "person")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Person {
-    private final boolean sex;
-    private final int age;
-    private final Contact contact;
-    private final String[] statuses;
+
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+
+    @XmlAttribute
+    private boolean sex;
+
+    @XmlAttribute
+    private int age;
+    private Contact contact;
+
+    @XmlElementWrapper
+    @XmlElement(name = "status")
+    private String[] statuses;
+
+    public Person() {
+    }
 
     public Person(boolean sex, int age, Contact contact, String... statuses) {
         this.sex = sex;
@@ -24,4 +47,22 @@ public class Person {
                 + ", statuses=" + Arrays.toString(statuses)
                 + '}';
     }
+
+    public static void main(String[] args) throws JAXBException {
+
+        final Person person = new Person(false, 30, new Contact("11-111"), "Worker", "Married");
+
+        JAXBContext context = JAXBContext.newInstance(Person.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+        try (StringWriter writer = new StringWriter()) {
+            marshaller.marshal(person, writer);
+            String result = writer.getBuffer().toString();
+            System.out.println(result);
+        } catch (Exception e) {
+            LOG.error("What went wrong", e);
+        }
+    }
+
 }
