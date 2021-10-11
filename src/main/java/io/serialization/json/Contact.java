cbc297f21847506627aaf8f3a7fa0c74cb539022
Айдер <emage.haf@gmail.com>
2021-10-11 12:02:01 +0300

    5. Преобразование JSON в POJO. JsonObject [#315064]

--- src/main/java/io/serialization/json/Contact.java
+++ src/main/java/io/serialization/json/Contact.java
@@ -1,7 +1,12 @@
 package io.serialization.json;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
+import java.util.ArrayList;
+import java.util.List;
 
 @XmlRootElement(name = "contact")
 public class Contact {
@@ -16,10 +21,35 @@ public class Contact {
         this.phone = phone;
     }
 
+    public String getPhone() {
+        return phone;
+    }
+
     @Override
     public String toString() {
         return "Contact{"
                 + "phone='" + phone + '\''
                 + '}';
+    }
+
+    public static void main(String[] args) {
+
+        JSONObject jsonContact = new JSONObject("{\"phone\":\"+7(924)111-111-11-11\"}");
+
+        List<String> list = new ArrayList<>();
+        list.add("Student");
+        list.add("Free");
+        JSONArray jsonStatuses = new JSONArray(list);
+
+        final Person person = new Person(false, 30, new Contact("11-111"), "Worker", "Married");
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("sex", person.isSex());
+        jsonObject.put("age", person.getAge());
+        jsonObject.put("contact", jsonContact);
+        jsonObject.put("statuses", jsonStatuses);
+
+        System.out.println(jsonObject);
+
+        System.out.println(new JSONObject(person));
     }
 }
